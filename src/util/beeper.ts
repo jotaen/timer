36071862476889980522c4afd2c49b70cab4b034
Jan Heuermann <jan@jotaen.net>
2026-02-13 16:32:07 +0100

    Capture audio context

--- src/util/beeper.ts
+++ src/util/beeper.ts
@@ -1,24 +1,27 @@
 export class Beeper {
-  beep(frequency: number, duration: number) {
-    const audioCtx = new (
+  private audioCtx: AudioContext | undefined
+
+  constructor() {
+    this.audioCtx = new (
       window.AudioContext ||
       (window as any).webkitAudioContext ||
       (window as any).audioContext
     )()
-    if (!audioCtx) {
+  }
+
+  beep(frequency: number, duration: number) {
+    if (!this.audioCtx) {
       return
     }
 
-    const oscillator = audioCtx.createOscillator()
-    const gainNode = audioCtx.createGain()
+    const oscillator = this.audioCtx.createOscillator()
+    const gainNode = this.audioCtx.createGain()
     oscillator.connect(gainNode)
-    gainNode.connect(audioCtx.destination)
-
+    gainNode.connect(this.audioCtx.destination)
     gainNode.gain.value = 0.5 // volume
     oscillator.frequency.value = frequency
     oscillator.type = "square"
-
-    oscillator.start(audioCtx.currentTime)
-    oscillator.stop(audioCtx.currentTime + duration / 1000)
+    oscillator.start(this.audioCtx.currentTime)
+    oscillator.stop(this.audioCtx.currentTime + duration / 1000)
   }
 }
