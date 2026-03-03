66112e1f8e51e6f5a45647bdf1e5612f20d3ecd4
Jan Heuermann <jan@jotaen.net>
2026-03-03 18:32:53 +0100

    Add more logging

--- src/util/beeper.ts
+++ src/util/beeper.ts
@@ -32,8 +32,9 @@ export class Beeper {
     gainNode.gain.value = 0.5 // volume
     oscillator.frequency.value = frequency
     oscillator.type = "square"
-    oscillator.start(this.audioCtx.currentTime)
-    oscillator.stop(this.audioCtx.currentTime + duration / 1000)
-    console.debug("Audio context: beep issued")
+    const t = this.audioCtx.currentTime + 0.01
+    oscillator.start(t)
+    oscillator.stop(t + duration / 1000)
+    console.debug(`Audio context: beep issued (f=${frequency}, d=${duration}), t=${t}`)
   }
 }
