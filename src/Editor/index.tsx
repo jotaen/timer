1036b0aba7dd521326fc7083a6b4847aeccaf0ed
Jan Heuermann <jan@jotaen.net>
2026-03-06 09:55:31 +0100

    Add syntax explainer

--- src/Editor/index.tsx
+++ src/Editor/index.tsx
@@ -25,6 +25,7 @@ export function Editor({
   const [text, setText] = useState<string>("")
   const [title, setTitle] = useState<string>("")
   const [parseError, setParseError] = useState<ParseError | Error | null>(null)
+  const [isSyntaxRulesShown, setIsSyntaxRulesShown] = useState<boolean>(true)
   const navigationGuard = useNavigationGuard()
 
   useEffect(() => {
@@ -116,6 +117,30 @@ export function Editor({
         disabled={isReadonly}
         placeholder="Program"
       ></textarea>
+      <div className={css.syntaxRules}>
+        <strong onClick={() => setIsSyntaxRulesShown(!isSyntaxRulesShown)}>
+          {isSyntaxRulesShown ? "⏷" : "⏵"} Syntax Rules
+        </strong>
+        {isSyntaxRulesShown && (
+          <p>
+            A timer program is processed line by line, where each line denotes
+            either an activity or a loop.
+            <h4>Activity</h4>
+            An activity is expressed by a time value, optionally followed by a
+            description (separated by one space character). The time value must
+            be formatted <code>M:SS</code> or <code>MM:SS</code> (minutes,
+            seconds). Examples: <code>0:45</code>, <code>1:00 Work Out!</code>.
+            <br />
+            If the time value is followed by an asterisk (e.g.,{" "}
+            <code>0:45*</code>), the activity is skipped on the last loop
+            iteration.
+            <h4>Loop</h4>A loop is expressed as repetition count, e.g.{" "}
+            <code>2x</code>, denoting that the following block of indented lines
+            shall be repeated that many times. Indentation is 2&nbsp;space
+            characters. Loops can be nested.
+          </p>
+        )}
+      </div>
     </div>
   )
 }
